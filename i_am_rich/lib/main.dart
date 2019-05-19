86d3bfa5079c38429b98da2cf5a71fadcf25569c
Dulio Denis <dulio.denis@yahoo.com>
2019-05-19 15:58:43 -0400

    Added a Centered NetworkImage Widget to the Scaffold body

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -7,6 +7,13 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')),
         ),
       ),
     ),
