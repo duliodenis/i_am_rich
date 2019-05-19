b05c6e562214949c3901dde173d79cc3cfccb65e
Dulio Denis <dulio.denis@yahoo.com>
2019-05-19 15:33:20 -0400

    Added Scaffold and AppBar Widgets

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(
-        child: Text("Hello, World!"),
+// The main() function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+        ),
       ),
-    ));
+    ),
+  );
+}
